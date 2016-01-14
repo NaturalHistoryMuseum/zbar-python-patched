0b1a24159accbce7256533db2009a7cc91b015cf
Lawrence Hudson <lawrence@edited.com>
2016-01-14 19:41:49 +0000

    Wheel

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
-from distutils.core import setup, Extension
+import os
+import shutil
+import sys
 
-setup(
+from setuptools import setup, Extension
+
+from distutils.sysconfig import get_config_vars
+
+setup_args = dict(
     name = 'zbar',
     version = '0.10',
     author = 'Jeff Brown',
@@ -42,3 +48,15 @@ setup(
         ),
     ],
 )
+
+if 'bdist' in sys.argv and 'win32' == sys.platform:
+    # More work for binary distruction of windows build
+    ZBAR = os.getenv('ZBAR') or 'C:\\ZBar'
+    shutil.copy(os.path.join(ZBAR, 'bin', 'zlib1.dll'), '.')
+    shutil.copy(os.path.join(ZBAR, 'bin', 'libzbar-0.dll'), '.')
+    shutil.copy(os.path.join(ZBAR, 'bin', 'libiconv-2.dll'), '.')
+    setup_args['ext_modules'][0].library_dirs = [os.path.join(ZBAR, 'lib')]
+    setup_args['ext_modules'][0].include_dirs = get_config_vars('INCLUDEPY') + [os.path.join(ZBAR, 'include')]
+    setup_args['data_files'] = [('', ['zlib1.dll', 'libzbar-0.dll', 'libiconv-2.dll'])]
+
+setup(**setup_args)
